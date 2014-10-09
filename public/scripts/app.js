13adedce6d7d8c4d394b027eb3ec3a2a413b5f8c
Kristoffer Robin Canlas <robin.canlas@geekgig.com>
2014-10-09 16:00:44 +0800

    Third commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,14 +13,14 @@ App.addInitializer(function(){
 
 App.addInitializer(function(){
 	var sidebars = new Sidebars([
-		{name: 'Home', data:'data-home'},
-		{name: 'Movies', data:'data-movies'},
-		{name: 'Genres', data:'data-genres'},
-		{name: 'Cinema', data:'data-cinema'},
-		{name: 'Featured', data:'data-featured'},
-		{name: 'Years', data:'data-years'},
-		{name: 'Countries', data:'data-countries'},				
-		{name: 'Languages', data:'data-languages'},
+		{name: 'Home', data:'data-home', id:1},
+		{name: 'Movies', data:'data-movies', id:2},
+		{name: 'Genres', data:'data-genres', id:3},
+		{name: 'Cinema', data:'data-cinema', id:4},
+		{name: 'Featured', data:'data-featured', id:5},
+		{name: 'Years', data:'data-years', id:6},
+		{name: 'Countries', data:'data-countries', id:7},				
+		{name: 'Languages', data:'data-languages', id:8},
 	]);
 
 	this.sidebarView = new Names({collection:sidebars});
@@ -70,8 +70,6 @@ App.addInitializer(function(){
 
 });
 
-App.on('start', function() {	
-	// console.log('app start');
-});
+App.on('start', function(){});
 
 App.start();
